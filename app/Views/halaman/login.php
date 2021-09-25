1f95173b8dfcc6a19762cf7b2cd96a4fad7ba0b8
NailahKhalilahPutri <nailahputri99@gmail.com>
2021-09-25 17:09:28 +0700

    tugas pertemuan 2

---
+++ app/Views/halaman/login.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Perpustakaan Login</title>
+    </head>
+    <body>
+        
+    <h1>Selamat datang di Sistem Informasi Perpustakaan</h1>
+    <p>
+        Ini adalah layanan perpustakaan digital masa kini
+        <a href="<?=base_url('/daftar-member')?>">klik daftar</a> untuk mendaftar menjadi member
+    </p>
+
+    </body>
+</html>
